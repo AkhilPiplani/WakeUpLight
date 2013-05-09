7ac7303fb4aad7d0f1c8efc9b47863d50422d40e
Akhil <akhil.piplani@gmail.com>
2013-05-09 11:30:56 +0200

    increased stack size and main tests timing

--- WakeUpLight/main.c
+++ WakeUpLight/main.c
@@ -12,89 +12,26 @@
 #include "utils/uartstdio.h"
 #include "lcd44780_LP.h"
 #include "commons.h"
+#include "time.h"
 
 char PrintString[64] = {0};
 
 unsigned char FillLed(unsigned int h,unsigned int j);
 
 int main(void) {
-	unsigned int i = 0;
+	Time time = {sunday, 23, 59, 45, 0};
 
-	ROM_SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN); // 400MHz / 2 / 4 (SYSCTL_SYSDIV_4) = 50MHz
+	ROM_SysCtlClockSet(SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN); // 400MHz / 2 / 5 (SYSCTL_SYSDIV_5) = 40MHz
 
 	//ROM_IntMasterEnable();
 
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-    ROM_GPIOPinConfigure(GPIO_PA0_U0RX);
-    ROM_GPIOPinConfigure(GPIO_PA1_U0TX);
-    ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
-    UARTStdioInit(0);
-    UARTprintf(" Test Print \n");
+    LCD_init();
 
-	LCD_init();
-
-	/*LCD_command(0x80);
-	LCD_buildCustomCharacters();
-
-	LCD_write(0);
-	LCD_write(1);
-	LCD_write(2);
-	LCD_write(3);
-	LCD_write(4);
-	LCD_write(5);
-	LCD_write(6);
-	LCD_write(7);*/
-
-	LCD_writeText("Hello world!", 0, 0);
-
-	ROM_SysCtlDelay(ROM_SysCtlClockGet() / 10/ 2 );
+	Time_init();
+	Time_set(&time);
 
 	while(1) {
-		sprintf(PrintString, "%u", i);
-		ROM_SysCtlDelay(ROM_SysCtlClockGet() / 10/ 2 );
-		LCD_writeText(PrintString, 0, 0);
-		i++;
+		ROM_SysCtlDelay(ROM_SysCtlClockGet()/10);
+		Time_printCurrentOnLCD();
 	}
-
 }
-
-
-
-
-
-unsigned char FillLed(unsigned int h,unsigned int j)
-{
-	if (h > 7)
-	{
-
-		if (j == 0)
-		{
-         	//some math here
-			//get the different
-			return h - 8;
-
-		}
-		else
-		{
-			return 7;  //max on second row
-		}
-	}
-	else
-	{
-		if (j == 0)
-		{
-			return ' ';   //leave row 0 blank, not power enough
-		}
-		else
-		{
-
-			return h;  //whatever the power of the led
-		}
-
-	}
-
-
-}
-
-
-
